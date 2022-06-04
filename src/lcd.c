9d1849f66d77e517bced620ed174796f2309a9fd
EstherWI <esthersss09@gmail.com>
2022-06-04 11:19:20 -0300

    gpio readall

--- src/lcd.c
+++ src/lcd.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 #include <wiringPi.h>
 #include <lcd.h>
-// lcdInit (int rows, int cols, int bits, int rs, int strb, int d0, int d1, int d2, int d3, int d4, int d5, int d6, int d7)
-// d0 - d7 pin data
-// rs - gpio 25 - pin 22
-// strb - gpio ID_SC - pin 28
+// list wiringPi-RPi pins $ gpio readall
 
 int main(){
     int fd = lcdInit(2, 16, 4, 6, 31, 26, 27, 28, 29,40,0,0,0,0);
