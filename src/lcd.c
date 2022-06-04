6650dbf0eed865a3938a90b62ebf1d9d7fdb7ccb
Anderson correia <40068482+AndersonCorreia@users.noreply.github.com>
2022-06-04 13:34:57 -0300

    Update lcd.c

--- src/lcd.c
+++ src/lcd.c
@@ -4,8 +4,28 @@
 // list wiringPi-RPi pins $ gpio readall
 
 int main(){
-    int fd = lcdInit(2, 16, 4, 6, 31, 26, 27, 28, 29,40,0,0,0,0);
+    int state = 1;
+    int lcd;
+    float temperatura=22.45, umidade= 84.6, luminosidade=39.369, pressao=14.233;
 
-    lcdPuts(fd,"hello world");
+    wiringPiSetup();
+    lcd = lcdInit(2,16,4,6,31,26,27,28,29,0,0,0,0);
+
+    //resetLcd(lcd);
+    lcdPrintf(lcd,"%.1f C %.1f I %.1f U %1.f Pa", temperatura, luminosidade, umidade, pressao);
+
+
     return 0;
+}
+
+
+
+
+void resetPosLcd(int lcd, int x, int y){
+    lcdClear(lcd);
+    lcdPosition(lcd, x, y);
+}
+
+void resetLcd(int lcd){
+    resetPosLcd(lcd, 0, 0);
 }
