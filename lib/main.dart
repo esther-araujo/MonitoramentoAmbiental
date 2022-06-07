b51f77c6cb4a64e43f7acbeed7d9f2ee297e2bc5
EstherWI <esthersss09@gmail.com>
2022-06-07 19:18:01 -0300

    Update interface

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,297 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(
+    const MaterialApp(
+      home: ExampleStaggeredAnimations(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class ExampleStaggeredAnimations extends StatefulWidget {
+  const ExampleStaggeredAnimations({
+    super.key,
+  });
+
+  @override
+  _ExampleStaggeredAnimationsState createState() =>
+      _ExampleStaggeredAnimationsState();
+}
+
+class _ExampleStaggeredAnimationsState extends State<ExampleStaggeredAnimations>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _drawerSlideController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _drawerSlideController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 150),
+    );
+  }
+
+  @override
+  void dispose() {
+    _drawerSlideController.dispose();
+    super.dispose();
+  }
+
+  bool _isDrawerOpen() {
+    return _drawerSlideController.value == 1.0;
+  }
+
+  bool _isDrawerOpening() {
+    return _drawerSlideController.status == AnimationStatus.forward;
+  }
+
+  bool _isDrawerClosed() {
+    return _drawerSlideController.value == 0.0;
+  }
+
+  void _toggleDrawer() {
+    if (_isDrawerOpen() || _isDrawerOpening()) {
+      _drawerSlideController.reverse();
+    } else {
+      _drawerSlideController.forward();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: _buildAppBar(),
+      body: Stack(
+        children: [
+          _buildContent(),
+          _buildDrawer(),
+        ],
       ),
-      home: MyHomePage(title: 'Monitoramento'),
+    );
+  }
+
+  PreferredSizeWidget _buildAppBar() {
+    return AppBar(
+      title: const Text(
+        'Monitoramento',
+        style: TextStyle(
+          color: Colors.black,
+        ),
+      ),
+      backgroundColor: Colors.transparent,
+      elevation: 0.0,
+      automaticallyImplyLeading: false,
+      actions: [
+        AnimatedBuilder(
+          animation: _drawerSlideController,
+          builder: (context, child) {
+            return IconButton(
+              onPressed: _toggleDrawer,
+              icon: _isDrawerOpen() || _isDrawerOpening()
+                  ? const Icon(
+                      Icons.clear,
+                      color: Colors.black,
+                    )
+                  : const Icon(
+                      Icons.menu,
+                      color: Colors.black,
+                    ),
+            );
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget _buildContent() {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blueGrey,
+        ),
+        home: MyHomePage(title: 'Monitoramento'));
+  }
+
+  Widget _buildDrawer() {
+    return AnimatedBuilder(
+      animation: _drawerSlideController,
+      builder: (context, child) {
+        return FractionalTranslation(
+          translation: Offset(1.0 - _drawerSlideController.value, 0.0),
+          child: _isDrawerClosed() ? const SizedBox() : const Menu(),
+        );
+      },
+    );
+  }
+}
+
+class Menu extends StatefulWidget {
+  const Menu({super.key});
+
+  @override
+  _MenuState createState() => _MenuState();
+}
+
+class _MenuState extends State<Menu> with SingleTickerProviderStateMixin {
+  static const _menuTitles = ['Configurações', 'Histórico', 'Sensores'];
+
+  static const _initialDelayTime = Duration(milliseconds: 50);
+  static const _itemSlideTime = Duration(milliseconds: 250);
+  static const _staggerTime = Duration(milliseconds: 50);
+  static const _buttonDelayTime = Duration(milliseconds: 150);
+  static const _buttonTime = Duration(milliseconds: 500);
+  final _animationDuration = _initialDelayTime +
+      (_staggerTime * _menuTitles.length) +
+      _buttonDelayTime +
+      _buttonTime;
+
+  late AnimationController _staggeredController;
+  final List<Interval> _itemSlideIntervals = [];
+  late Interval _buttonInterval;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _createAnimationIntervals();
+
+    _staggeredController = AnimationController(
+      vsync: this,
+      duration: _animationDuration,
+    )..forward();
+  }
+
+  void _createAnimationIntervals() {
+    for (var i = 0; i < _menuTitles.length; ++i) {
+      final startTime = _initialDelayTime + (_staggerTime * i);
+      final endTime = startTime + _itemSlideTime;
+      _itemSlideIntervals.add(
+        Interval(
+          startTime.inMilliseconds / _animationDuration.inMilliseconds,
+          endTime.inMilliseconds / _animationDuration.inMilliseconds,
+        ),
+      );
+    }
+
+    final buttonStartTime =
+        Duration(milliseconds: (_menuTitles.length * 50)) + _buttonDelayTime;
+    final buttonEndTime = buttonStartTime + _buttonTime;
+    _buttonInterval = Interval(
+      buttonStartTime.inMilliseconds / _animationDuration.inMilliseconds,
+      buttonEndTime.inMilliseconds / _animationDuration.inMilliseconds,
+    );
+  }
+
+  @override
+  void dispose() {
+    _staggeredController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          _buildFlutterLogo(),
+          _buildContent(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildFlutterLogo() {
+    return Positioned(
+      right: -300,
+      bottom: -250,
+      child: Opacity(
+        opacity: 0.2,
+        child: SvgPicture.asset('svg/Raspberry_Pi_Logo.svg',
+      fit: BoxFit.scaleDown
+         ),
+      ),
+      
+    );
+  }
+
+  Widget _buildContent() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(height: 16),
+        ..._buildListItems(),
+        const Spacer(),
+        _buildGetStartedButton(),
+      ],
+    );
+  }
+
+  List<Widget> _buildListItems() {
+    final listItems = <Widget>[];
+    for (var i = 0; i < _menuTitles.length; ++i) {
+      listItems.add(
+        AnimatedBuilder(
+          animation: _staggeredController,
+          builder: (context, child) {
+            final animationPercent = Curves.easeOut.transform(
+              _itemSlideIntervals[i].transform(_staggeredController.value),
+            );
+            final opacity = animationPercent;
+            final slideDistance = (1.0 - animationPercent) * 150;
+
+            return Opacity(
+              opacity: opacity,
+              child: Transform.translate(
+                offset: Offset(slideDistance, 0),
+                child: child,
+              ),
+            );
+          },
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 36.0, vertical: 16),
+            child: Text(
+              _menuTitles[i],
+              textAlign: TextAlign.left,
+              style: const TextStyle(
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+        ),
+      );
+    }
+    return listItems;
+  }
+
+  Widget _buildGetStartedButton() {
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: AnimatedBuilder(
+          animation: _staggeredController,
+          builder: (context, child) {
+            final animationPercent = Curves.elasticOut.transform(
+                _buttonInterval.transform(_staggeredController.value));
+            final opacity = animationPercent.clamp(0.0, 1.0);
+            final scale = (animationPercent * 0.5) + 0.5;
+
+            return Opacity(
+              opacity: opacity,
+              child: Transform.scale(
+                scale: scale,
+                child: child,
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
@@ -37,13 +313,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        leading: Icon(
-          Icons.troubleshoot,
-          color: Colors.white,
-        ),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
